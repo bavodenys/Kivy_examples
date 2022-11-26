e466824c04da7f798c34bd5d6b91ef0097586f5f
bavodenys <66639415+bavodenys@users.noreply.github.com>
2022-11-26 09:30:28 +0100

    Create gen_functions.py

---
+++ WorkOutTimer/gen_functions.py
@@ -0,0 +1,8 @@
+import os
+
+# Function to convert duration in seconds -> mm:ss
+def convert_s_to_min_s_str(dur_s):
+    minutes = int(dur_s/60)
+    seconds = int(dur_s%60)
+    result = f'{minutes:02d}:{seconds:02d}'
+    return result
