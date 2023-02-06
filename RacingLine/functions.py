bf7167b821c06e9bd3e6e01d7428b872378e91da
bavo.denys <bavodenys@gmail.com>
2023-02-06 20:52:10 +0100

    Commit before making race track

--- RacingLine/functions.py
+++ RacingLine/functions.py
@@ -1,5 +1,25 @@
+from calibrations import *
+from math import sin, cos, radians
 
+# Function to calculate the acceleration
 def calculate_acceleration(acceleration_force, braking_force, veh_spd):
+    # Drag force
     drag_force = (AIR_DENSITY*DRAG_COEFF*FRONTAL_AREA*veh_spd*veh_spd)/2
+    # Rolling resistance
+
+    # Calculate acceleration
     acceleration = (acceleration_force-braking_force-drag_force)/(VEHICLE_MASS + DRIVER_MASS)
     return acceleration
+
+# Function to calculate the vehicle speed
+def calculate_speed(vehicle_speed_old, vehicle_acceleration, delta_t):
+    vehicle_speed_new = vehicle_speed_old + (vehicle_acceleration*delta_t)
+    return vehicle_speed_new
+
+# Calculate the vehicle position
+def calculate_position(vehicle_pos_x, vehicle_pos_y, vehicle_speed, steering_angle, orientation_angle, delta_t):
+
+    resulting_angle = orientation_angle - steering_angle
+    vehicle_pos_y_new = vehicle_pos_y + (vehicle_speed*delta_t)*sin(radians(resulting_angle))*MAP_SCALING
+    vehicle_pos_x_new = vehicle_pos_x + (vehicle_speed*delta_t)*cos(radians(resulting_angle))*MAP_SCALING
+    return vehicle_pos_x_new, vehicle_pos_y_new, resulting_angle
--- RacingLine/main.py
+++ RacingLine/main.py
@@ -3,31 +3,22 @@ from kivy.core.window import Window
 from kivy.graphics.vertex_instructions import Ellipse
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.graphics import Color
 from kivy.clock import Clock
 from functions import *
+from calibrations import *
+
 
 # Version
 MAJOR_VERSION = 0
 MINOR_VERSION = 1
 
-# Calibrations
-START_POS_X = 200
-START_POS_Y = 200
-ELLIPSE_RADIUS = 20
-VEHICLE_MASS =
-DRIVER_MASS =
-AIR_DENSITY =
-DRAG_COEFF =
-FRONTAL_AREA =
-ACCELERATING_FORCE =
-BRAKING_FORCE =
-
-
 # Set window to screen size
 Window.maximize()
 
 class MainWindow(MDBoxLayout):
+    dashboard_speed = StringProperty('')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -38,7 +29,14 @@ class MainWindow(MDBoxLayout):
         self.key_down_active = False
         self.key_right_active = False
         self.key_left_active = False
+        self.vehicle_pos_x = MAX_POS_X/2
+        self.vehicle_pos_y = MAX_POS_Y/2
+        self.orientation_angle = 90
         self.vehicle_speed = 0
+        self.vehicle_acceleration = 0
+        self.accelerating_force = 0
+        self.braking_force = 0
+        self.steering_angle = 0
         # Create the racing track
         with self.canvas:
             Color(1, 1, 1)
@@ -48,15 +46,41 @@ class MainWindow(MDBoxLayout):
     def update(self, dt):
         # Remove the vehicle from the canvas
         self.canvas.remove(self.vehicle)
-        self.accelerating_force = ACCELERATING_FORCE if self.key_up_active else self.accelerating_force = 0
-        self.braking_force = BRAKING_FORCE if self.key_down_active else self.braking_force = 0
-        self.acceleration = calculate_acceleration(self.accelerating_force, self.braking_force, self.vehicle_speed)
+
+        self.accelerating_force = ACCELERATING_FORCE if self.key_up_active else 0
+        self.braking_force = BRAKING_FORCE if self.key_down_active else 0
+        if self.key_left_active and self.key_right_active:
+            pass
+        else:
+            if self.key_left_active:
+                if self.steering_angle <= -MAX_STEERING_ANGLE:
+                    pass
+                else:
+                    self.steering_angle -= ANGLE_INCREASE
+            if self.key_right_active:
+                if self.steering_angle >= MAX_STEERING_ANGLE:
+                    pass
+                else:
+                    self.steering_angle += ANGLE_INCREASE
+
+        # Calculate vehicle acceleration
+        self.vehicle_acceleration = calculate_acceleration(self.accelerating_force, self.braking_force, self.vehicle_speed)
+        # Calculate vehicle speed
+        self.vehicle_speed = calculate_speed(self.vehicle_speed, self.vehicle_acceleration, dt)
+        # calculate vehicle position
+        self.vehicle_pos_x, self.vehicle_pos_y, self.orientation_angle = calculate_position(self.vehicle_pos_x,
+                                                                                            self.vehicle_pos_y,
+                                                                                            self.vehicle_speed,
+                                                                                            self.steering_angle,
+                                                                                            self.orientation_angle,
+                                                                                            dt)
 
 
         with self.canvas:
-            self.vehicle = Ellipse(pos=[START_POS_X, START_POS_Y], size=(ELLIPSE_RADIUS, ELLIPSE_RADIUS))
+            self.vehicle = Ellipse(pos=[self.vehicle_pos_x, self.vehicle_pos_y], size=(ELLIPSE_RADIUS, ELLIPSE_RADIUS))
 
-
+        # Dashboard variables
+        self.dashboard_speed = str(int(self.vehicle_speed * 3.6))
 
     def on_keyboard_down(self, instance, keyboard, keycode, text, modifiers):
         if keycode == 82:  # Key UP
