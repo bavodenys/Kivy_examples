e2262932b806128188d9f8f77a2524d344d24a2f
bavo.denys <bavodenys@gmail.com>
2023-08-26 09:59:44 +0200

    adding function to determine distance to the walls

--- MicroMouse/functions.py
+++ MicroMouse/functions.py
@@ -1,11 +1,15 @@
 import random
 from calibrations import *
+import math
 
 def generate_maze(rows, cols):
     maze = [[C_CELL_ALL_WALLS for _ in range(cols)] for _ in range(rows)]
 
     start_row = random.randint(0, rows - 1)
     start_col = random.randint(0, cols - 1)
+
+    # Save the start_row, start_col as finish
+    finish = [start_row, start_col]
 
     # Mark the starting cell as visited
     maze[start_row][start_col] = maze[start_row][start_col] | C_CELL_VISITED
@@ -51,4 +55,42 @@ def generate_maze(rows, cols):
         else:
             stack.pop()
 
-    return maze
+    return maze, finish
+
+# Function to return distance sensor values: up, left, right, down
+def get_distance_sensor_values(mouse_pos_abs, maze):
+    # Determine the position in the maze with as reference the upper left corner
+    mouse_pos_rel_x = mouse_pos_abs[0] - MAZE_REF_X
+    mouse_pos_rel_y = MAZE_REF_Y - mouse_pos_abs[1]
+    # Determine the cell the mouse is in
+    index_x = math.floor(mouse_pos_rel_x / MAZE_BLOCK_SIZE)
+    index_y = math.floor(mouse_pos_rel_y / MAZE_BLOCK_SIZE)
+    # Determine the relative position in a cell
+    remain_x = mouse_pos_rel_x % MAZE_BLOCK_SIZE
+    remain_y = mouse_pos_rel_y % MAZE_BLOCK_SIZE
+    # Determine distance up
+    distance_up = remain_y - MOUSE_SIZE_Y/2 - WALL_THICKNESS
+    i = 0
+    while (not(maze[index_y-i][index_x] & C_CELL_TOP == C_CELL_TOP)):
+        distance_up = distance_up + MAZE_BLOCK_SIZE
+        i += 1
+    # Determine distance down
+    distance_down = (MAZE_BLOCK_SIZE - remain_y) - MOUSE_SIZE_Y/2 - WALL_THICKNESS
+    i = 0
+    while (not(maze[index_y+i][index_x] & C_CELL_BOTTOM == C_CELL_BOTTOM)):
+        distance_down = distance_down + MAZE_BLOCK_SIZE
+        i += 1
+    # Determine distance right
+    distance_right = (MAZE_BLOCK_SIZE - remain_x) - MOUSE_SIZE_X/2 - WALL_THICKNESS
+    i = 0
+    while (not(maze[index_y][index_x+i] & C_CELL_RIGHT == C_CELL_RIGHT)):
+        distance_right = distance_right + MAZE_BLOCK_SIZE
+        i += 1
+    # Determine distance left
+    distance_left = remain_x - MOUSE_SIZE_X/2 - WALL_THICKNESS
+    i = 0
+    while (not(maze[index_y][index_x-i] & C_CELL_LEFT == C_CELL_LEFT)):
+        distance_left = distance_left + MAZE_BLOCK_SIZE
+        i += 1
+    # Return distances
+    return distance_up, distance_down, distance_right, distance_left
--- MicroMouse/main.py
+++ MicroMouse/main.py
@@ -29,12 +29,13 @@ class MicroMouse(Widget):
         self.mouse_widgets = []
         super().__init__(**kwargs)
         Color(0,0,1)
-        self.ellipse = Ellipse(pos=(self.pos_x-MOUSE_SIZE/2,self.pos_y-MOUSE_SIZE/2), size=(MOUSE_SIZE, MOUSE_SIZE))
+        self.ellipse = Ellipse(pos=(self.pos_x-MOUSE_SIZE_X/2,self.pos_y-MOUSE_SIZE_Y/2), size=(MOUSE_SIZE_X, MOUSE_SIZE_Y))
 
     # Update the ellipse on the canvas
     def update_pos(self):
         Color(0,0,1)
-        self.ellipse = Ellipse(pos=(self.pos_x - MOUSE_SIZE / 2, self.pos_y - MOUSE_SIZE / 2), size=(MOUSE_SIZE, MOUSE_SIZE))
+        self.ellipse = Ellipse(pos=(self.pos_x - MOUSE_SIZE_X / 2, self.pos_y - MOUSE_SIZE_Y / 2), size=(MOUSE_SIZE_X, MOUSE_SIZE_Y))
+
 
 
 class MainWindow(MDBoxLayout):
@@ -42,39 +43,42 @@ class MainWindow(MDBoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Clock.schedule_interval(self.update, 1 / 60)
-        maze = generate_maze(MAZE_BLOCKS_X, MAZE_BLOCKS_Y)
-        for i in range(MAZE_BLOCKS_X):
-            for j in range(MAZE_BLOCKS_Y):
-                maze[i][j] = maze[i][j] - 64
+        # Generate the maze
+        self.maze, finish = generate_maze(MAZE_BLOCKS_X, MAZE_BLOCKS_Y)
+        #for i in range(MAZE_BLOCKS_X):
+        #    for j in range(MAZE_BLOCKS_Y):
+        #        self.maze[i][j] = self.maze[i][j] - 64
 
         self.maze_widgets = []
-        # Create the racing track
+        # Create the maze
         with self.canvas:
             self.mouse = MicroMouse(START_POS_X_MOUSE, START_POS_Y_MOUSE)
             Color(1, 1, 0)  #Yellow
             for j in range(MAZE_BLOCKS_Y):
                 for i in range(MAZE_BLOCKS_X):
                     # Set finish of the maze
-                    if maze[j][i] & C_CELL_FINISH == C_CELL_FINISH:
+                    if self.maze[j][i] & C_CELL_FINISH == C_CELL_FINISH:
                         Color(0, 1, 0)
-                        self.maze_widgets.append(Rectangle(pos=[START_X + i*MAZE_BLOCK_SIZE,START_Y - MAZE_BLOCK_SIZE - j*MAZE_BLOCK_SIZE], size=[MAZE_BLOCK_SIZE, MAZE_BLOCK_SIZE]))
+                        self.maze_widgets.append(Rectangle(pos=[MAZE_REF_X + i*MAZE_BLOCK_SIZE,MAZE_REF_Y - MAZE_BLOCK_SIZE - j*MAZE_BLOCK_SIZE], size=[MAZE_BLOCK_SIZE, MAZE_BLOCK_SIZE]))
                         Color(1, 1, 0)
                     # Set start of the maze
                     if i == 0 and j == MAZE_BLOCKS_Y - 1:
                         Color(1,0,0)
-                        self.maze_widgets.append(Rectangle(pos=[START_X + i*MAZE_BLOCK_SIZE,START_Y - MAZE_BLOCK_SIZE - j*MAZE_BLOCK_SIZE], size=[MAZE_BLOCK_SIZE, MAZE_BLOCK_SIZE]))
+                        self.maze_widgets.append(Rectangle(pos=[MAZE_REF_X + i*MAZE_BLOCK_SIZE,MAZE_REF_Y - MAZE_BLOCK_SIZE - j*MAZE_BLOCK_SIZE], size=[MAZE_BLOCK_SIZE, MAZE_BLOCK_SIZE]))
                         Color(1, 1, 0)
-                    if maze[j][i] & C_CELL_BOTTOM == C_CELL_BOTTOM:
-                        self.maze_widgets.append(Rectangle(pos=[START_X + i*MAZE_BLOCK_SIZE, START_Y - MAZE_BLOCK_SIZE - j*MAZE_BLOCK_SIZE], size=[MAZE_BLOCK_SIZE,WALL_THICKNESS]))
-                    if maze[j][i] & C_CELL_RIGHT == C_CELL_RIGHT:
-                        self.maze_widgets.append(Rectangle(pos=[START_X + MAZE_BLOCK_SIZE-WALL_THICKNESS + i * MAZE_BLOCK_SIZE, START_Y -MAZE_BLOCK_SIZE - j * MAZE_BLOCK_SIZE],size=[WALL_THICKNESS, MAZE_BLOCK_SIZE]))
-                    if maze[j][i] & C_CELL_LEFT == C_CELL_LEFT:
-                        self.maze_widgets.append(Rectangle(pos=[START_X + i * MAZE_BLOCK_SIZE, START_Y - MAZE_BLOCK_SIZE - j * MAZE_BLOCK_SIZE],size=[WALL_THICKNESS, MAZE_BLOCK_SIZE]))
-                    if maze[j][i] & C_CELL_TOP == C_CELL_TOP:
-                        self.maze_widgets.append(Rectangle(pos=[START_X + i * MAZE_BLOCK_SIZE, START_Y - WALL_THICKNESS - j * MAZE_BLOCK_SIZE],size=[MAZE_BLOCK_SIZE, WALL_THICKNESS]))
+                    if self.maze[j][i] & C_CELL_BOTTOM == C_CELL_BOTTOM:
+                        self.maze_widgets.append(Rectangle(pos=[MAZE_REF_X + i*MAZE_BLOCK_SIZE, MAZE_REF_Y - MAZE_BLOCK_SIZE - j*MAZE_BLOCK_SIZE], size=[MAZE_BLOCK_SIZE,WALL_THICKNESS]))
+                    if self.maze[j][i] & C_CELL_RIGHT == C_CELL_RIGHT:
+                        self.maze_widgets.append(Rectangle(pos=[MAZE_REF_X + MAZE_BLOCK_SIZE-WALL_THICKNESS + i * MAZE_BLOCK_SIZE, MAZE_REF_Y -MAZE_BLOCK_SIZE - j * MAZE_BLOCK_SIZE],size=[WALL_THICKNESS, MAZE_BLOCK_SIZE]))
+                    if self.maze[j][i] & C_CELL_LEFT == C_CELL_LEFT:
+                        self.maze_widgets.append(Rectangle(pos=[MAZE_REF_X + i * MAZE_BLOCK_SIZE, MAZE_REF_Y - MAZE_BLOCK_SIZE - j * MAZE_BLOCK_SIZE],size=[WALL_THICKNESS, MAZE_BLOCK_SIZE]))
+                    if self.maze[j][i] & C_CELL_TOP == C_CELL_TOP:
+                        self.maze_widgets.append(Rectangle(pos=[MAZE_REF_X + i * MAZE_BLOCK_SIZE, MAZE_REF_Y - WALL_THICKNESS - j * MAZE_BLOCK_SIZE],size=[MAZE_BLOCK_SIZE, WALL_THICKNESS]))
 
     # Update
     def update(self, dt):
+        distance_up, distance_down, distance_right, distance_left = get_distance_sensor_values([self.mouse.pos_x, self.mouse.pos_y], self.maze)
+        print(f" UP:{distance_up}, DOWN: {distance_down}, RIGHT: {distance_right}, LEFT: {distance_left}")
         self.canvas.remove(self.mouse.ellipse)
         with self.canvas:
             self.mouse.update_pos()
